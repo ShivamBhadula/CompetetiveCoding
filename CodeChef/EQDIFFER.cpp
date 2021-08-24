6fe03ecba0708030bc45420fcf8aeb15ed18547c
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-08-24 23:37:46 +0530

    August-Cook-Off-2021

---
+++ CodeChef/EQDIFFER.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    int m=0;
+	    map<int,int> mp;
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	        mp[a[i]]++;
+	        m=max(m,mp[a[i]]);
+	    }
+	    if(n<=2)
+	    {
+	        cout<<0<<endl;
+	        continue;
+	    }
+	    else if (m==1)
+	    {
+	        cout<<n-2<<endl;
+	        continue;
+	    }
+	 cout<<n-m<<endl;   
+	}
+	return 0;
+}
