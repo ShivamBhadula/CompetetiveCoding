1310d4c1dc12ead92eaff3922a210d6617168d3b
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-09-02 14:39:52 +0530

    Delete Hackerearth directory

--- Hackerearth/Code Monk/Minimum AND xor OR.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    length=int(input())
-    l1=list(map(int,input().split()))
-    l1.sort()
-    xor=l1[0]^l1[1]
-    for i in range(1,length-1):
-                val= l1[i]^l1[i+1]
-                xor=min(xor,val)
-    print(xor)
