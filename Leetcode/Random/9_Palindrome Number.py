ad876dc8479143b56acd313a4584d118d4c219b2
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-09-23 23:43:42 +0530

    Create 9_Palindrome Number.py

---
+++ Leetcode/Random/9_Palindrome Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x=str(x)
+        x1=x[::-1]
+        if x==x1:
+            return True
+        else:
+            return False
+        
