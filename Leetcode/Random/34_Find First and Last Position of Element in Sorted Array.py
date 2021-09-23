9aa8b8b3bf9ff4e85f8ba1f3543ee039f4631f89
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-09-23 23:44:55 +0530

    Create 34_Find First and Last Position of Element in Sorted Array.py

---
+++ Leetcode/Random/34_Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,43 @@
+def binary_search(lo, hi, condition):
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = condition(mid)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return -1
+
+def first_position(nums, target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid > 0 and nums[mid-1] == target:
+                return 'left'
+            return 'found'
+        elif nums[mid] < target:
+            return 'right'
+        else:
+            return 'left'
+    return binary_search(0, len(nums)-1, condition)
+
+def last_position(nums, target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid < len(nums)-1 and nums[mid+1] == target:
+                return 'right'
+            return 'found'
+        elif nums[mid] < target:
+            return 'right'
+        else:
+            return 'left'
+    return binary_search(0, len(nums)-1, condition)
+
+def first_and_last_position(nums, target):
+    return first_position(nums, target), last_position(nums, target)
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        return first_and_last_position(nums,target)
+        
