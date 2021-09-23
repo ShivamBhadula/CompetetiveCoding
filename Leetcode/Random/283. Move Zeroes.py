509e6aecc75ea12aa2775c339907df6c1afba243
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-09-23 23:51:07 +0530

    Create 283. Move Zeroes.py

---
+++ Leetcode/Random/283. Move Zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero=[]
+        for i in nums:
+            if i==0:
+                nums.remove(i)
+                nums.append(0)
+                
+                
