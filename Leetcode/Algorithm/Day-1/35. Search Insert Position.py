68fdb73cf0807b417744d9fc7c03f1598367c17a
Shivam Bhadula <86179239+ShivamBhadula@users.noreply.github.com>
2021-09-26 11:49:56 +0530

    Create 35. Search Insert Position.py

---
+++ Leetcode/Algorithm/Day-1/35. Search Insert Position.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low=0
+        high=len(nums)
+        
+        while low<high:
+            mid=(low+high)//2
+            
+            
+            if nums[mid]>=target:
+                high=mid
+            
+            else:
+                low=mid+1
+                
+        return low
+                
+                
+                
+                
